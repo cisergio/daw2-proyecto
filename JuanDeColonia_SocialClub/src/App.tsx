0af99879e3bf584d62a14f17051a511867540883
cisergio <sergioalonsolop@gmail.com>
2026-03-04 18:27:25 +0100

    Implementado TailwindCss junto a inicio de sesion sencillo

--- JuanDeColonia_SocialClub/src/App.tsx
+++ JuanDeColonia_SocialClub/src/App.tsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { useState, useEffect } from "react";
+import { onAuthStateChanged, signOut } from "firebase/auth";
+import { auth } from "./firebase/config";
+import type { User } from "firebase/auth";
+import Login from "./pages/login";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [user, setUser] = useState<User | null>(null);
+  const [cargando, setCargando] = useState(true);
+
+  const handleLogout = async () => {
+    await signOut(auth);
+  };
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (u) => {
+      setUser(u);
+      setCargando(false);
+    });
+  }, []);
+
+  if (cargando) return <p>Cargando...</p>;
+  if (!user) return <Login />;
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <button onClick={handleLogout}>Cerrar sesion</button>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- JuanDeColonia_SocialClub/src/pages/login.tsx
+++ JuanDeColonia_SocialClub/src/pages/login.tsx
@@ -18,11 +18,9 @@ export default function Login() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-50 ">
       <div className="p-8 border border-gray-200 bg-white rounded-lg shadow-md w-96 overflow-hidden">
-
         <h2 className="text-2xl font-bold text-center mb-6">Login</h2>
 
         <form onSubmit={handleLogin} className="space-y-4">
-          
           <input
             type="email"
             placeholder="Email"
@@ -40,7 +38,6 @@ export default function Login() {
           <button className="w-full bg-blue-500 text-white p-2 rounded-md hover:bg-blue-600">
             Entrar
           </button>
-          
         </form>
       </div>
     </div>
