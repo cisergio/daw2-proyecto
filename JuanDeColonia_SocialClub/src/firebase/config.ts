72e1f3f930722e3bcfa1fe41f758235914f4d685
smr012022 <smr012022@gmail.com>
2026-03-06 10:22:28 +0000

    deploy con login de prueba

--- JuanDeColonia_SocialClub/src/firebase/config.ts
+++ JuanDeColonia_SocialClub/src/firebase/config.ts
@@ -1,28 +1,23 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
-import { getFirestore, getDocs, addDoc } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
-
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB03CIs3C7x-za2VFMj6OwdK4RqczMz_DE",
-  authDomain: "proyectoprimerasemana.firebaseapp.com",
-  projectId: "proyectoprimerasemana",
-  storageBucket: "proyectoprimerasemana.firebasestorage.app",
-  messagingSenderId: "765557578274",
-  appId: "1:765557578274:web:09fc7f365261a4be2ac87b",
-  measurementId: "G-80YHXLC0LP",
+  apiKey: "AIzaSyDzz69a1qZIerFhy4JJGmxMxtZXGqcg7I0",
+  authDomain: "juandecolonia-socialclub.firebaseapp.com",
+  projectId: "juandecolonia-socialclub",
+  storageBucket: "juandecolonia-socialclub.firebasestorage.app",
+  messagingSenderId: "803457189688",
+  appId: "1:803457189688:web:28ce83bda2e000034ae3cd",
+  measurementId: "G-CK30LGT9XJ"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const firestore = getFirestore(app);
-const storage = getStorage(app);
-
-export { auth, app, firestore, getDocs, addDoc, storage };
+export const auth = getAuth(app);
+export const analytics = getAnalytics(app);
